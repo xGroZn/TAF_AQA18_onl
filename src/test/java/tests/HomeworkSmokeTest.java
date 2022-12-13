ed53ad734e71267e4666632988696624a164b56c
xGroZn <av_budnik@mail.ru>
2022-12-13 23:00:13 +0300

    add main homework task

--- src/test/java/tests/HomeworkSmokeTest.java
+++ src/test/java/tests/HomeworkSmokeTest.java
@@ -27,19 +27,39 @@ public class HomeworkSmokeTest {
         WebElement selectWebElementLayingMethod = driver.findElement(By.id("laying_method_laminate"));
         Select selectLayingMethodLaminate = new Select(selectWebElementLayingMethod);
         selectLayingMethodLaminate.selectByIndex(1);
-        driver.findElement(By.id("ln_room_id")).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(By.id("ln_room_id")).sendKeys(Keys.CONTROL + "a", Keys.DELETE);
         driver.findElement(By.id("ln_room_id")).sendKeys( "500");
-        driver.findElement(By.id("wd_room_id")).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(By.id("wd_room_id")).sendKeys(Keys.CONTROL + "a", Keys.DELETE);
         driver.findElement(By.id("wd_room_id")).sendKeys("400");
-        driver.findElement(By.id("ln_lam_id")).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(By.id("ln_lam_id")).sendKeys(Keys.CONTROL + "a", Keys.DELETE);
         driver.findElement(By.id("ln_lam_id")).sendKeys("2000");
-        driver.findElement(By.id("wd_lam_id")).sendKeys(Keys.CONTROL + "a");
+        driver.findElement(By.id("wd_lam_id")).sendKeys(Keys.CONTROL + "a", Keys.DELETE);
         driver.findElement(By.id("wd_lam_id")).sendKeys("200");
         driver.findElement(By.id("direction-laminate-id1")).click();
         driver.findElement(By.linkText("Рассчитать")).click();
         Thread.sleep(4000);
         Assert.assertEquals(driver.findElement(By.cssSelector("[style='padding:5px 0;font-size:22px; color:#C80303; font-weight:bold;']")).getText(), "52");
         Assert.assertEquals(driver.findElement(By.cssSelector("[style='padding:5px 0;font-size:18px; color:#0E8C19; font-weight:bold;']")).getText(), "7");
+    }
+
+    @Test
+    public void validateWarmFloorCalculatorTest() throws InterruptedException {
+        driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+
+        WebElement RoomType = driver.findElement(By.id("room_type"));
+        WebElement HeatingType = driver.findElement(By.id("heating_type"));
+
+        Select selectRoomType = new Select(RoomType);
+        Select selectHeatingType = new Select(HeatingType);
+
+        driver.findElement(By.id("el_f_width")).sendKeys("10");
+        driver.findElement(By.id("el_f_lenght")).sendKeys("15");
+        selectRoomType.selectByVisibleText("Кухня или спальня");
+        selectHeatingType.selectByIndex(1);
+        driver.findElement(By.name("button")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.findElement(By.id("floor_cable_power")).getAttribute("value"), "18000");
+        Assert.assertEquals(driver.findElement(By.id("spec_floor_cable_power")).getAttribute("value"), "120");
     }
 
     @AfterMethod
