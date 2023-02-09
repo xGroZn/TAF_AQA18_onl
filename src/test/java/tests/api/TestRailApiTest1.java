55647f755d645ec86050cfcc76c6e1735dfd7dd6
xGroZn <av_budnik@mail.ru>
2023-02-09 20:07:33 +0300

    restApi

--- src/test/java/tests/api/TestRailApiTest1.java
+++ src/test/java/tests/api/TestRailApiTest1.java
@@ -2,17 +2,21 @@ package tests.api;
 
 import baseEntities.BaseApiTest;
 import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
 import models.Project;
 import org.apache.http.HttpStatus;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class TestRailApiTest1 extends BaseApiTest {
+
+    int projectID;
 
     @Test
     public void addProject1() {
@@ -85,4 +89,72 @@ public class TestRailApiTest1 extends BaseApiTest {
                 .log().body()
                 .statusCode((HttpStatus.SC_OK));
     }
+
+    @Test
+    public void addProject4() {
+        String endpoint = "/index.php?/api/v2/add_project";
+
+        Project expectedProject = new Project();
+        expectedProject.setName("WP_Project_04");
+        expectedProject.setAnnouncement("This is a description!!!");
+        expectedProject.setType(1);
+        expectedProject.setShowAnnouncement(true);
+
+        projectID = given()
+                .body(expectedProject, ObjectMapperType.GSON)
+                .when()
+                .post(endpoint)
+                .then()
+                .log().body()
+                .statusCode((HttpStatus.SC_OK))
+                .extract()
+                .jsonPath()
+                .getInt("id");
+
+        System.out.println(projectID);
+    }
+
+    @Test
+    public void addProject5() {
+        String endpoint = "/index.php?/api/v2/add_project";
+
+        Project expectedProject = new Project();
+        expectedProject.setName("WP_Project_05");
+        expectedProject.setAnnouncement("This is a description!!!");
+        expectedProject.setType(1);
+        expectedProject.setShowAnnouncement(true);
+
+        Response response = given()
+                .body(expectedProject, ObjectMapperType.GSON)
+                .when()
+                .post(endpoint)
+                .then()
+                .log().all()
+                .statusCode((HttpStatus.SC_OK))
+                .extract()
+                .response();
+
+        projectID = response.getBody().jsonPath().get("id");
+
+        Assert.assertEquals(response.getBody().jsonPath().getString("name"),
+                expectedProject.getName());
+    }
+
+    @Test(dependsOnMethods = "addProject5")
+    public void readProject() {
+        String endpoint = "index.php?/api/v2/get_project/{project_id}";
+
+        Response response = given()
+                .pathParams("project_id", projectID)
+                .log().all()
+                .when()
+                .get(endpoint)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("id", equalTo(projectID))
+                //.body("name", is(expectedProject.getName())) //застрял в 20.00, этот тест не проходит, что-то с expectedProject
+                .extract().response();
+    }
+
 }
